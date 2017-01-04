33700916ed7196c4bebd4ad5b8485afa92fd383f
xueyongGao <xueyongGao@localhost>
2017-01-04 17:16:24 +0800

    删除多余代码 Signed-off-by: xueyongGao <xueyongGao@localhost>

--- src/main/java/com/sp/web/SysOfficeController.java
+++ src/main/java/com/sp/web/SysOfficeController.java
@@ -10,7 +10,6 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
